9a3e0394e6898dbd9c5c8421366343d8e6ab37b0
mettroid <jorik006006@mail.ru>
2023-04-25 23:22:53 +1200

    fix(plume): убрать шлейф змейки

--- src/modules/food.mjs
+++ src/modules/food.mjs
@@ -2,8 +2,10 @@ import {Sprite} from './sprite.mjs'
 class Food extends Sprite{
     draw(ctx){
         ctx.fillStyle = this.color;
+        ctx.beginPath();
         ctx.rect(this.x * this.cell, this.y * this.cell, this.w, this.h);
         ctx.fill();
+
     }
 }
 export {Food}
