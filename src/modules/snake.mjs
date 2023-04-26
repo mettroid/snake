d93b2db3379eb86487b31863d7b9af3d8047869c
mettroid <jorik006006@mail.ru>
2023-04-26 23:47:43 +1200

    feat(snake): добавить динамический массив объектов с координатами змейки

--- src/modules/snake.mjs
+++ src/modules/snake.mjs
@@ -4,26 +4,29 @@ class Snake extends Sprite{
     rightPressed = false;
     downPressed = false;
     upPressed = false;
-    dx = 1;
-    dy = 1;
+    trail = [];
+    tail = 3;
     draw(ctx, canvas){
         this.checkKey();
         ctx.fillStyle = this.color;
         ctx.beginPath(); //head
-        ctx.rect(this.x * this.cell, this.y * this.cell, this.w, this.h);
+        for(let i = 0; i < this.trail.length; i++){
+            ctx.rect(this.trail[i].x * this.cell, this.trail[i].y * this.cell, this.w, this.h);
+        }
+        this.trail.push({x: this.x, y: this.y});
+        if(this.trail.length > 3){
+            this.trail.shift();
+        }
         ctx.fill();
     }
     checkKey(){
         if(this.rightPressed && this.x < 29){
             this.x += 1;
-        }
-        if(this.leftPressed && this.x > 0){
+        }else if(this.leftPressed && this.x > 0){
             this.x -= 1;
-        }
-        if(this.upPressed && this.y > 0){
+        }else if(this.upPressed && this.y > 0){
             this.y -= 1;
-        }
-        if(this.downPressed && this.y < 29){
+        }else if(this.downPressed && this.y < 29){
             this.y += 1;
         }
     }
