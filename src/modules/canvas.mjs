c68bdc81f8db627ebc172a220254213bebbe8443
mettroid <jorik006006@mail.ru>
2023-04-29 19:42:54 +1200

    refactor(canvas): Добавить canvas в конец родителя, переназначить высоту

--- src/modules/canvas.mjs
+++ src/modules/canvas.mjs
@@ -11,10 +11,10 @@ class Canvas {
         } else {
             this.canvas = document.createElement('canvas');
             this.canvas.width = this.parent.offsetWidth-2;
-            this.canvas.height = this.parent.offsetHeight-2;
+            this.canvas.height = 600;
             this.canvas.id = this.id;  
             
-            this.parent.insertAdjacentElement('afterBegin', this.canvas);
+            this.parent.insertAdjacentElement('beforeEnd', this.canvas);
             this.ctx = this.canvas.getContext('2d');
         }
     }
