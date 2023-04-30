fd02aad23c712e6a5857cccbf96d38b6ea13e06f
mettroid <jorik006006@mail.ru>
2023-04-30 20:33:09 +1200

    refactot(snake): Переписать проще метод checkCrashSelf

--- src/modules/snake.mjs
+++ src/modules/snake.mjs
@@ -9,16 +9,21 @@ class Snake extends Sprite{
     tail = 3;
     draw(ctx, food){
         this.checkKey();
-        ctx.fillStyle = this.color;
-        ctx.beginPath(); //head
-        for(let i = 0; i <= this.trail.length - 1; i++){
-            ctx.rect(this.trail[i].x * this.cell, this.trail[i].y * this.cell, this.w, this.h);
-            if(i === this.trail.length - 1){ this.#checkCrashSelf(i, i) }
-        }
         this.trail.push({x: this.x, y: this.y});
         if(this.trail.length > this.tail){
             this.trail.shift();
         }
+        ctx.fillStyle = this.color;
+        ctx.beginPath(); //head
+        for(let i = 0; i < this.trail.length; i++){
+            ctx.rect(this.trail[i].x * this.cell, this.trail[i].y * this.cell, this.w, this.h);
+            if(this.#checkCrashSelf(i, this.trail.length - 1)){
+                this.trail.splice(0, this.trail.length - 3);
+                this.tail = 3;
+                break;
+            }
+        }
+
         this.eat(food);
         ctx.fill();
         ctx.stroke();
@@ -26,20 +31,17 @@ class Snake extends Sprite{
     checkCrashWall(){
         let endObj = this.trail[this.trail.length-1];
         if(endObj.x < 0 || 
-           endObj.x > 30 ||
+           endObj.x > 29 ||
            endObj.y < 0 ||
-           endObj.y > 30){ 
+           endObj.y > 29){ 
                 return true;
         }
     }
-    #checkCrashSelf(pos, length){
-    outer: for(let j = 0; j < length; j++){
-                if(isEqual(this.trail[j], this.trail[pos])){
-                    this.trail.splice(0, this.trail.length - 3);
-                    this.tail = 3;
-                    break outer;
+    #checkCrashSelf(pos, len){
+                if(pos == len) return false;
+                if(isEqual(this.trail[pos], this.trail[len])){
+                    return true;
                 }
-            }
     }
     checkKey(){
         if(this.rightPressed){
